60ea5e7d462fa9f05f103f00ec9bd6d096e39511
cwj <chenwj.yz@foxmail.com>
2020-02-11 19:43:29 +0800

    更改邮件

--- src/main/java/cn/cwj/community/util/MailUtil.java
+++ src/main/java/cn/cwj/community/util/MailUtil.java
@@ -31,15 +31,18 @@ public class MailUtil {
     }
     public static Boolean sendAuthCodeEmail(String userEmail, String authCode) {
         HtmlEmail email=new HtmlEmail();//创建一个HtmlEmail实例对象
+
         email.setHostName("smtp.163.com");//邮箱的SMTP服务器，一般123邮箱的是smtp.123.com,qq邮箱为smtp.qq.com
         email.setCharset("utf-8");//设置发送的字符类型
         try {
             System.out.println("用户邮件为："+userEmail);
+            email.setSmtpPort(465);
             email.addTo(userEmail);//设置收件人
             email.setFrom("chenweijun_yz@163.com","FreeMi");//发送人的邮箱为自己的，用户名可以随便填
             email.setAuthentication("chenweijun_yz@163.com","123456cwj");//设置发送人到的邮箱和用户名和授权码(授权码是自己设置的)
             email.setSubject("FreeMi官方邮箱验证码");//设置发送主题
             String mailHtml = getMailHtml(authCode, new Date().toString());
+            System.out.println(email.getSmtpPort());
             email.setMsg(mailHtml);//设置发送内容
             email.send();//进行发送
             return true;
